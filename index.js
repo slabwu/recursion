0810bdd9f91d5ed1072b16e3a9a15bb0a386ff5e
slabwu <134193743+slabwu@users.noreply.github.com>
2025-01-04 13:21:17 +0800

    Implement iterative fibonacci function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("test")
+function fibs(n) {
+    let a, b, c, output;
+    a = 0;
+    b = 1;
+    output = [];
+
+    if (n >= 2) {
+        output.push(0, 1)
+    } else {
+        return [ 0 ];
+    }
+
+    for (let i = 0; i < n - 2; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        output.push(c);
+    }
+
+    return output;
+}
+
+console.log(fibs(8));
