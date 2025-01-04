7c200e70bee010b184b7006a5293a0c30fac4197
slabwu <134193743+slabwu@users.noreply.github.com>
2025-01-04 21:08:30 +0800

    Split fibonacci & merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,36 +1,3 @@
-function fibs(n) {
-    let a, b, c, output;
-    a = 0;
-    b = 1;
-    output = [];
-
-    if (n >= 2) {
-        output.push(0, 1)
-    } else {
-        return [ 0 ];
-    }
-
-    for (let i = 0; i < n - 2; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-        output.push(c);
-    }
-
-    return output;
-}
-
-function fibsRec(n) {
-    if (n < 2) return [0];
-    if (n < 3) return [0, 1];
-    let output = fibsRec(n - 1);
-    output.push(output[n - 2] + output[n - 3]);
-    return output;
-}
-
-console.log(fibs(8));
-console.log(fibsRec(8));
-
 function mergeSort(array) {
     if (array.length === 1) return array;
     let left = array;
