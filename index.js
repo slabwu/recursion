11c2f4d32d97c1b0cf9d177cfb44e6a50d40a7fb
slabwu <134193743+slabwu@users.noreply.github.com>
2025-01-04 13:46:12 +0800

    Implement recursive fibonacci function

--- index.js
+++ index.js
@@ -20,4 +20,25 @@ function fibs(n) {
     return output;
 }
 
+function fibsRec(n) {
+    let output = [];
+
+    let fib = n => {
+        if (n === 1) {
+            return 0;
+        } else if (n === 2 || n === 3) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    for (let i = 1; i < n + 1; i++) {
+        output.push(fib(i));
+    }
+
+    return output;
+}
+
 console.log(fibs(8));
+console.log(fibsRec(8));
